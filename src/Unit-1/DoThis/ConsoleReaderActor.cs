017e1f10c09ad9ae9c6f890cdf92c780becda522
Russell Dillin <rastreus@gmail.com>
2019-02-02 16:31:03 -0800

    Lesson 1 edits
    These edits are from reading through https://github.com/rastreus/akka-bootcamp/blob/master/src/Unit-1/lesson1/README.md

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -10,6 +10,8 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+        public const string ContinueCommand = "continue";
+
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -29,10 +31,10 @@ namespace WinTail
             }
 
             // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
+            _consoleWriterActor.Tell(read);
 
             // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
+            Self.Tell(ContinueCommand);
         }
 
     }
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-﻿using Akka.Actor;
+using Akka.Actor;
 
 namespace WinTail
 {
     #region Program
     class Program
     {
+        public const string StartCommand = "start";
+
         public static ActorSystem MyActorSystem;
 
         static void Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create(nameof(MyActorSystem));
 
             PrintInstructions();
 
-            // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
-            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
 
+            // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
+            var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(consoleWriterActor)));
 
             // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
+            consoleReaderActor.Tell(StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
